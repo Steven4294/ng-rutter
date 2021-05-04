2858d14a8a6831e6059381fda6151f655381758e
Steven Petteruti <stevenpetterutijr@gmail.com>
2021-05-03 20:50:59 -0400

    update version

--- dist/esm5/public_api.js
+++
@@ -1,7 +0,0 @@
-/**
- * @fileoverview added by tsickle
- * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
- */
-export { NgRutterModule, FOR_ROOT_OPTIONS_TOKEN, provideMyServiceOptions } from './src/app/modules/ng-rutter/ng-rutter.module';
-
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicHVibGljX2FwaS5qcyIsInNvdXJjZVJvb3QiOiJuZzovL25nLXJ1dHRlci8iLCJzb3VyY2VzIjpbInB1YmxpY19hcGkudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7OztBQUFBLGdGQUFjLDhDQUE4QyxDQUFBIiwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0ICogZnJvbSAnLi9zcmMvYXBwL21vZHVsZXMvbmctcnV0dGVyL25nLXJ1dHRlci5tb2R1bGUnIl19
